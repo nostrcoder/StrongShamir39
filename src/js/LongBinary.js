6d2648c36da70e31001e38a46c8bc01ddfe4cd67
nostrcoder <137230846+nostrcoder@users.noreply.github.com>
2023-07-27 21:07:26 +0000

    Replaced GF(8) by BigGF based on LongBinary, introduced StrongShamir39, optimized mnemonic length, introduced bip39- and StrongShamir39 checksums

---
+++ src/js/LongBinary.js
@@ -0,0 +1,238 @@
+class LongBinary {
+  constructor(value, bits) {
+    // create with value 0
+    if (value == undefined) {
+      this.array = [false];
+    } else {
+      this.set(value, bits);
+    }
+  }
+
+  // TODO: test
+  // value: string of 0 and 1, or an integer value, or an array of booleans
+  // bits (optional): the number of bits in the representation
+  set(value, bits) {
+    if (typeof value === "string") {
+      this._readString(value);
+    } else if (typeof value === "number" && Number.isInteger(value) && value >= 0) {
+      this._readInt(value);
+    } else if (Array.isArray(value) && value.every(x => typeof x === 'boolean')) {
+      this.array = value;
+    } else {
+      throw "Value " + value + " is not correct for LongBinary"
+    }
+
+    // process 2nd parameter
+    if (bits != undefined) {
+      // adjust the array to the length = bits
+      if (bits > this.array.length) {
+        // add bits and fill with zeros
+        let arr2 = new Array(bits - this.array.length).fill(false);
+        this.array = this.array.concat(arr2);
+      } else if (bits < this.array.length) {
+        // shorten array
+        this.array = this.array.slice(0, bits - 1);
+      }
+    }
+
+    return this;
+  }
+
+  copy() {
+    return new LongBinary(this.array);
+  }
+
+  _readInt(value) {
+    if (value == 0) {
+      this.array = [false];
+      return this;
+    }
+
+    this.array = [];
+    while (value > 0) {
+      this.array.push(value & 1 ? true : false);
+      value >>= 1
+    }
+    return this;
+  }
+
+  _readString(str) {
+    if (/[^01]/.test(str)) {
+      throw "Value " + str + "is not correct for LongBinary"
+    }
+    this.array = str.split('').map(value => value == '1').reverse();
+    return this;
+  }
+
+  toString() {
+    return this.toBinString();
+  }
+
+  toBinString() {
+    return this.array.map(value => value ? "1" : "0").reverse().join("");
+  }
+
+  toBoolArray() {
+    return this.array.slice();
+  }
+
+  getBit(height) {
+    if (height >= this.array.length) {
+      return 0;
+    } else {
+      return (this.array[height] ? 1 : 0);
+    }
+  }
+
+  setBit(height) {
+    var res = this.copy();
+    if (height < res.array.length) {
+      res.array[height] = true;
+    } else {
+      var arr2 = new Array(height - res.array.length + 1).fill(false);
+      arr2[arr2.length - 1] = true;
+      res.array = res.array.concat(arr2);
+    }
+    return res;
+  }
+
+  // Not needed?
+  /*  
+    padWithZero(bits) {
+      var res = new LongBinary();
+      var pad = new Array(bits).fill(false);
+      res.array = this.array.concat(pad);
+      return res;
+    }
+  */
+
+
+  // UNARY OPERATORS: not, inc, dec
+
+  not() {
+    var res = new LongBinary();
+    res.array = this.array.map(value => !value);
+
+    return res;
+  }
+
+  lshift(bits) { // bits is int
+    if (bits == undefined) {
+      bits = 1;
+    };
+    var res = new LongBinary();
+    res.array = new Array(bits).fill(false);
+    res.array = res.array.concat(this.array);
+
+    return res;
+  }
+
+  rshift(bits) { // bits is int
+    if (bits == undefined) {
+      bits = 1;
+    };
+    var res = new LongBinary();
+    res.array = this.array.slice(bits, this.array.length);
+    if (res.array.length == 0) res.set(0);
+
+    return res;
+  }
+
+  // splits this.array and lb into 3 parts
+  // used in bit operations with 2 operands
+  // op1 - is the beginning of this.array upto the common length
+  // op2 - is the bebinning of lb upto the common length
+  // rest - is the rest either of this.array or lb
+  _split4ops(lb) {
+    let minLength = Math.min(this.array.length, lb.array.length);
+    let op1 = this.array.slice(0, minLength);
+    let op2 = lb.array.slice(0, minLength);
+    let rest = [];
+    if (lb.array.length > minLength) {
+      rest = lb.array.slice(minLength, lb.array.length);
+    } else if (this.array.length > minLength) {
+      rest = this.array.slice(minLength, this.array.length);
+    }
+    return [op1, op2, rest]; // op1, op2, rest are boolean lists
+  }
+
+  // BINARY OPERATORS: xor, or, and
+
+  xor(lb) {
+    let res = new LongBinary();
+
+    let [op1, op2, rest] = this._split4ops(lb);
+    res.array = op1.map((value, index) => Boolean(value ^ op2[index]));
+    res.array = res.array.concat(rest);
+
+    return res;
+  }
+
+  or(lb) {
+    let res = new LongBinary();
+
+    let [op1, op2, rest] = this._split4ops(lb);
+    res.array = op1.map((value, index) => Boolean(value || op2[index]));
+    res.array = res.array.concat(rest);
+
+    return res;
+  }
+
+  and(lb) {
+    let res = new LongBinary();
+
+    let [op1, op2, rest] = this._split4ops(lb);
+    res.array = op1.map((value, index) => Boolean(value && op2[index]));
+    rest = new Array(rest.length).fill(false);
+    res.array = res.array.concat(rest);
+
+    return res;
+  }
+
+  getLength() {
+    return this.array.length;
+  }
+
+  // Methods for interpretation as a Number
+
+  // what's the degree of the polynomial represented by lb?
+  // what's the highest power of 2 in lb?
+  getDegree() {
+    let x = this.array.length - 1;
+    while (x > 0) {
+      if (this.array[x]) return x; // returns integer
+      x = x - 1;
+    }
+    return 0; // integer
+  }
+
+  normalize() {
+    let res = new LongBinary();
+    res.array = this.array.slice(0, this.getDegree() + 1);
+    return res; // LongBinary    
+  }
+
+  // pad with 0 to the size
+  pad(size) {
+    let res = this.copy();
+
+    if (size <= this.array.length) {
+      // ATTENTION!!! Potentially cuting out some significant bits!
+      res.array = this.array.slice(0, size);
+    } else {
+      let arr2 = new Array(size - this.array.length).fill(false);
+      res.array = res.array.concat(arr2);
+    }
+
+    return res;
+  }
+
+  isZero() {
+    let x = this.normalize();
+    return (x.getDegree() == 0 && !x.array[0]); // boolean
+  }
+
+}
+
+// for node.js:
+// module.exports = LongBinary;
---
+++ src/js/SSS.js
@@ -0,0 +1,339 @@
+// for node.js:
+//const GF = require('./BigGF.js');
+const GF = BigGF;
+
+// TODO: improve randomness
+function randomElement(gf) {
+  let degree = gf.degree;
+  let res = gf.getElement(0);
+  for (var i = 0; i < degree; i++) {
+    if (Math.round(Math.random()) == 1) {
+      res = res.setBit(i);
+    }; // biased to 0!
+  }
+  return res;
+}
+
+// TODO: add array of 01 to LongBinary.set
+// This is the basic SSS function
+// secret can be:
+// - a 01-string
+// - an array of booleans
+// - an array of 01 (to be implemented in LongBinary.set)
+function getShares(secret, numShares, threshold) {
+
+  let degree = secret.length;
+  let gf = new GF(degree);
+
+  secret = gf.getElement(secret)
+
+  let shares = [];
+  let coeffs = [secret];
+
+  // set the coeffs of the solution polynomial (coeffs are elements of GF)
+  // f(x) = c[0] + c[1]*x + c[2]*x^2 + c[3]*x^3 + ... + c[n]*x^n, n=threshhold-1
+  // set c[0] = secret
+  // set c[i] randomly for all i>0
+  // it works, since then f(x) = c[0] = secret
+  // for threshold = 1 => n=0
+  // for threshold = 2 => n=1, linear polynomial
+  // for threshold = 3 => n=2, quadratic polynomial
+  for (let i = 1; i < threshold; i++) {
+    do {
+      coeffs[i] = randomElement(gf);
+    } while (i == threshold - 1 && coeffs[i].isZero()); //highest coeff must be non-zero
+    // otherwise less than threshhold shares suffice for reconstruction!
+    // so repeat if the highest coeff is zero
+  }
+  console.log('coeffs: ' + coeffs);
+
+  // compute the values of f(x) at the x-points 1,...,numShares
+  for (var i = 1, len = numShares + 1; i < len; i++) {
+    shares[i - 1] = {
+      x: i,
+      y: horner(gf.getElement(i), coeffs, gf).pad(gf.degree).toBinString()
+    }
+  }
+
+  return shares;
+}
+
+// evaluate the polynomial given by coeffs at x
+// note: coeffs are themselves elements of GF
+function horner(x, coeffs, gf) {
+  y = gf.getElement(0);
+  for (i = coeffs.length - 1; i > 0; i--) {
+    y = gf.add(y, coeffs[i]);
+    y = gf.mul(y, x);
+  };
+  y = gf.add(y, coeffs[0]);
+  return y;
+}
+
+/*
+
+console.log("GET SHARES")
+let secret = '00011100';
+console.log("secret: " + secret);
+let shares = getShares(secret, 3, 3);
+for (i = 0; i < shares.length; i++) {
+  console.log("share " + shares[i]['x'] + ': ' + shares[i]['y']);
+  //console.log(shares[i]['y']);
+}
+
+
+console.log()
+console.log("HORNER")
+
+function testHorner(x, coeffs) {
+  gf = new GF(8);
+  x = gf.getElement(x);
+  coeffs = coeffs.map(function(value) {
+    return gf.getElement(value);
+  });
+  console.log('x=' + x + ', coeffs=' + coeffs + ' => ' + horner(x, coeffs, gf))
+}
+
+testHorner(2, [4, 5]);
+testHorner(3, [1, 2, 3]);
+testHorner(255, [1, 0, 1, 0, 1, 0, 1, 1])
+testHorner(255, [1, 2, 3, 4, 5, 6, 7, 8])
+
+function zeroPolynomial(gf) {
+  return [gf.getElement(0)];
+}
+
+// a, b are polynomials with coefficients in the GF
+// represented as arrays of GF instances
+function addPolynomials(a, b, gf) {
+  if (a.length > b.length) {
+    //swap the arrays
+    let temp = a;
+    a = b;
+    b = temp;
+  }; // now b is longer than a
+
+  let res = b.slice()
+  for (let i = 0; i < a.length; i++) {
+    res[i] = gf.add(b[i], a[i]); // use the add function of the GF  
+  }
+
+  return res;
+}
+
+// multiplies Polynomial with the constant k in GF
+function simpleMulPoly(k,a, gf) {  
+  if(k.isZero()) return [gf.getElement(0)];
+  
+  let res = [];
+  for(let i = 0; i<a.length;i++) {
+    res[i] = gf.mul(a[i],k);
+  }
+  return res;
+}
+
+// a, b are polynomials with coefficients in the GF
+// represented as arrays of GF instances
+function mulPolynomials(a, b, gf) {
+//  console.log()
+//  console.log(a+" * "+b+':')
+  let h;
+  let product = [gf.getElement(0)]; // product = 0
+  while (b.length > 0) { // while b not a zero polynomial (empty array)
+    if (!b[0].isZero()) {
+      h = simpleMulPoly(b[0],a,gf)
+//    console.log('h='+h)
+      product = addPolynomials(product, h, gf);
+//    console.log('h='+h)    
+    }
+    a.unshift(gf.getElement(0)); // multiply by x, i.e. insert 0 at the beginning of the coeff array 
+    b.shift(); //take away the lowest coeff
+//    console.log('p='+product)
+//    console.log('a='+a)
+//    console.log('b='+b)
+  }
+  return normalizePolynomial(product);
+}
+
+function normalizePolynomial(a) {
+  let i=a.length;
+  while(a[i-1].isZero() && i>1) {
+    i--;
+  }
+  return a.slice(0,i);
+}
+
+console.log()
+console.log("ZERO POLYNOMIAL")
+myGF = new GF(8);
+zero = zeroPolynomial(myGF);
+console.log(zero);
+
+console.log()
+console.log("POLYNOMIAL ADDITION")
+function testAddPolynomials(a, b) {
+  console.log(a + ' + ' + b + ' = ' + addPolynomials(a, b, myGF))
+
+}
+
+function testAddRandomPolynomials() {
+  deg1 = Math.ceil(Math.random() * 3)
+  poly1 = []
+  for (let i = 0; i <= deg1; i++) {
+    poly1.push(myGF.getElement(Math.floor(Math.random() * 5)));
+  }
+
+  deg1 = Math.ceil(Math.random() * 3)
+  poly2 = []
+  for (let i = 0; i <= deg1; i++) {
+    poly2.push(myGF.getElement(Math.floor(Math.random() * 5)));
+  }
+
+  testAddPolynomials(poly1, poly2)
+}
+
+for (j = 0; j < 5; j++) {
+  testAddRandomPolynomials()
+}
+
+zero = gf.getElement(0);
+one = gf.getElement(1);
+two = gf.getElement(2);
+three = gf.getElement(3);
+
+console.log()
+console.log("SIMPLE POLYNOMIAL MULTIPLICATION")
+function testSimpleMul(a, k) {
+  console.log(a + ' * ' + k + ' = ' + simpleMulPoly(k, a, myGF))
+
+}
+
+testSimpleMul([one],zero);
+testSimpleMul([one],one);
+testSimpleMul([one,one],zero);
+testSimpleMul([zero,one],one);
+testSimpleMul([one,two],zero);
+testSimpleMul([zero,one],two);
+testSimpleMul([two,zero,one],two);
+testSimpleMul([two,zero,one],zero);
+
+
+console.log()
+console.log("POLYNOMIAL MULTIPLICATION")
+function testMulPolynomials(a, b) {
+  console.log(a + ' * ' + b + ' = ' + mulPolynomials(a, b, myGF))
+
+}
+
+function testMulRandomPolynomials() {
+  deg1 = Math.round(Math.random() * 3)
+  poly1 = []
+  for (let i = 0; i <= deg1; i++) {
+    poly1.push(myGF.getElement(Math.floor(Math.random() * 5)));
+  }
+
+  deg1 = Math.round(Math.random() * 3)
+  poly2 = []
+  for (let i = 0; i <= deg1; i++) {
+    poly2.push(myGF.getElement(Math.floor(Math.random() * 5)));
+  }
+
+  testMulPolynomials(poly1, poly2)
+}
+
+for (j = 0; j < 10; j++) {
+  testMulRandomPolynomials()
+}
+
+console.log()
+console.log("NORMALIZE POLYNOMIAL")
+
+function testNormalizePolynomial(a) {
+  console.log(a + " > " + normalizePolynomial(a))
+}
+
+testNormalizePolynomial([one]);
+testNormalizePolynomial([zero]);
+testNormalizePolynomial([zero,one]);
+testNormalizePolynomial([one,zero]);
+testNormalizePolynomial([zero,zero]);
+testNormalizePolynomial([zero,one,zero]);
+testNormalizePolynomial([zero,one,one]);
+testNormalizePolynomial([one,zero,zero]);
+testNormalizePolynomial([zero,zero,zero]);
+
+*/
+
+// points are formatted like the output for getShares
+// compute only the f(x) for x=0
+// meaning look at x^0 only
+// no polynomial multiplication needed!
+function lagrange(points) {
+
+  let degree = points[0]['y'].length;
+  let gf = new GF(degree);
+
+  let fAt0 = [];
+  let yAt0 = gf.getElement(0);
+  for (let i = 0; i < points.length; i++) {
+    fAt0[i] = gf.getElement(points[i]['y']);
+    //    console.log('fAt0['+i+']='+fAt0[i])
+    for (let j = 0; j < points.length; j++) {
+      if (i != j) {
+        let numerator = gf.getElement(points[j]['x']);
+        //        console.log('numerator='+numerator)
+        let denumerator = gf.sub(gf.getElement(points[i]['x']), gf.getElement(points[j]['x']));
+        //        console.log('denumerator='+denumerator)
+        numerator = gf.mul(numerator, gf.inv(denumerator));
+        //        console.log('term='+numerator)
+        fAt0[i] = gf.mul(fAt0[i], numerator);
+        //        console.log('fAt0['+i+']='+fAt0[i])    
+      }
+    }
+    yAt0 = gf.add(yAt0, fAt0[i]).pad(degree);
+    //    console.log('yAt0='+yAt0)    
+  }
+  return yAt0;
+}
+
+
+/*
+
+console.log()
+console.log("COMBINE")
+
+result = lagrange(shares);
+console.log("lagrange: " + result)
+
+function testShareAndCombine(degree) {
+  console.log()
+  console.log("SHARE & COMBINE")
+  secret = randomElement(new GF(degree)).pad(degree).toBinString();
+  console.log("secret:  " + secret);
+  let numshares = Math.floor(Math.random() * 4) + 3
+  let threshhold = Math.floor(Math.random() * (numshares - 4)) + 3
+  console.log(threshhold + ' out of ' + numshares);
+  let shares = getShares(secret, numshares, threshhold);
+  for (i = 0; i < shares.length; i++) {
+    console.log("share " + shares[i]['x'] + ': ' + shares[i]['y']);
+    //console.log(shares[i]['y']);
+  }
+  shares = shares.slice(0, threshhold + 1);
+  combination = lagrange(shares);
+  console.log('combine: ' + lagrange(shares));
+  //  secret = '0' + secret.substring(1);
+  if (secret == combination.toBinString()) {
+    console.log("SUCCESS!");
+  } else {
+    console.log("FAILED!")
+  };
+}
+
+for (let i = 0; i < 5; i++) {
+  testShareAndCombine(256);
+}
+
+*/
+
+// for node.js:
+// module.exports = { getShares, lagrange };
